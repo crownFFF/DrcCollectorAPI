6e5a6a5c57b15e56786c121049c72341666d25b5
crownFFF <asd8792323@gmail.com>
2024-08-02 13:48:57 +0800

    commit

--- exhibits-list.php
+++ exhibits-list.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin:*');
-header('Content-Type:application/json;chartset=utf-8');
+header('Content-Type:application/json;charset=utf-8');
 
 require_once('./connection.php');
 
